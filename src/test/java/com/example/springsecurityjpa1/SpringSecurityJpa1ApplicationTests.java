a36f336fb528c3d8f280cfe305c84d64ac87a7e7
sayone-63 <techatkevin@gmail.com>
2019-12-03 14:51:22 +0530

    simple sec with hardcoded

---
+++ src/test/java/com/example/springsecurityjpa1/SpringSecurityJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
